2bcad353891a0133e7075ee6775b0101dc194161
Kamil Kowalski <kamilkowalski314@gmail.com>
2022-04-10 23:39:49 +0200

    Helper mapper added

--- Helpers/Mapper.cs
+++ Helpers/Mapper.cs
@@ -1,10 +1,37 @@
+using System.Reflection;
+using System.Collections;
+
 namespace Rollcall.Helpers
 {
     //TODO
     public class Mapper
     {
-        /*public T MapProperties<T,Q>(Q q) where T  new{
-            return new T()
-        }*/
+        public IEnumerable<Tuple<PropertyInfo, PropertyInfo>> CreateMapping<T, Q>()
+        {
+            var dstFields = typeof(T).GetProperties();
+            var sourceType = typeof(Q);
+
+            var commonFields = new List<Tuple<PropertyInfo, PropertyInfo>>();
+
+            foreach (var dstField in dstFields)
+            {
+                var srcField = sourceType.GetProperty(dstField.Name);
+                if (srcField is not null && srcField.PropertyType.Equals(dstField.PropertyType))
+                {
+                    commonFields.Add(new Tuple<PropertyInfo,PropertyInfo>(dstField, srcField));
+                }
+            }
+            return commonFields;
+        }
+        public T MapProperties<T, Q>(Q q) where T : new()
+        {
+            var result = new T();
+            var mapping = CreateMapping<T, Q>();
+            foreach (Tuple<PropertyInfo,PropertyInfo> map in mapping)
+            {
+                map.Item1.SetValue(result, map.Item2.GetValue(q));
+            }
+            return result;
+        }
     }
 }
